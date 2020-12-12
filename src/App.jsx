e2744958b5e08f51a30c4eef7283cc853759443d
Navan Sami <navan.sami@yahoo.com>
2020-12-12 21:57:34 +0200

    created state to save search term to pass a back option

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./pages/Home";
 import DisplayUser from "./components/users/DisplayUser";
 
 const App = () => {
+  const [term, setTerm] = useState("");
+
+  const searchTerm = (searchTerm) => {
+    setTerm(searchTerm);
+  };
+
   const navbarOptions = {
     title: "GitFinder",
     theme: "dark", // theme options -> [dark, light, primary, danager, success]
@@ -22,7 +28,7 @@ const App = () => {
       </div>
 
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" render={() => <Home searchTerm={searchTerm} />} />
         <Route path="/user/:username" component={DisplayUser} />
       </Switch>
     </Router>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "../components/searchbar/SearchBar";
 import UserList from "../components/users/UserList";
 import useGitUsers from "../hooks/useGitUsers";
 
-const Home = ({ userSelected }) => {
+const Home = ({ searchTerm }) => {
+  const [term, setTerm] = useState("");
   const [usersList, search] = useGitUsers("");
+
+  const onSearchTerm = (term) => {
+    return searchTerm(term);
+  };
 
   return (
     <div className="container" style={{ padding: "10px 0" }}>
       <SearchBar onFormReceived={search} />
-      <UserList usersList={usersList} userSelected={userSelected} />
+      <UserList usersList={usersList} />
     </div>
   );
 };
